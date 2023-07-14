cdf5eef369240a3e846708d00f5e687c67601444
Kyle Crawford <kcraw217@gmail.com>
2023-07-14 16:02:15 -0700

    add null check

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
 import './styles/styles.css'
 
-ReactDOM.render(
+const element = document.getElementById('root');
+if (element === null) throw new Error('Root container missing in index.html');
+
+const root = ReactDOM.createRoot(element);
+
+root.render(
   <>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter >
     </React.StrictMode>
-  </>,
-  document.getElementById("root")
+  </>
 );
