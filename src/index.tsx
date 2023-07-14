f924517af358e37bc643dea4d9fe31e487a06ed1
Kyle Crawford <kcraw217@gmail.com>
2023-07-14 09:25:40 -0700

    add react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <>
-    <App />
-  </>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter >
+    </React.StrictMode>
+  </>,
+  document.getElementById("root")
 );
