33b3b858e28b9a2df0541403ba5c23f2ec859b02
Kyle Crawford <kcraw217@gmail.com>
2023-07-21 15:30:30 -0700

    add planes

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -3,25 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import gsap from 'gsap';
 
-type Shape = THREE.Mesh<THREE.BoxGeometry | THREE.SphereGeometry, THREE.MeshNormalMaterial | THREE.MeshStandardMaterial>
-
-function sphere(): Shape {
-  const geometry = new THREE.SphereGeometry(3, 64, 64);
-  const material = new THREE.MeshStandardMaterial({
-    color: "#00ff83"
-  });
-
-  return new THREE.Mesh(geometry, material);
-}
-
-function cube(): Shape {
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshNormalMaterial();
-  return new THREE.Mesh(geometry, material);
-}
 
 const initThreeJsScene = (node: HTMLDivElement) => {
   const scene = new THREE.Scene();
+  const group = new THREE.Group();
 
   const sizes = {
     width: window.innerWidth,
@@ -39,19 +24,40 @@ const initThreeJsScene = (node: HTMLDivElement) => {
 
   camera.position.z = 10;
 
-  //The Shape
-  const geometry = new THREE.SphereGeometry(3, 64, 64);
-  const material = new THREE.MeshStandardMaterial({
+  //The Shapes
+  const sphereGeometry = new THREE.SphereGeometry(3, 64, 64);
+  const sphereMaterial = new THREE.MeshStandardMaterial({
     color: "#00ff83"
   });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  group.add(sphere);
 
-  const mesh = new THREE.Mesh(geometry, material);
+  const planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
+  const planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x6904ce,
+    side: THREE.DoubleSide,
+    wireframe: true
+  });
 
-  scene.add(mesh);
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = 0.5 * Math.PI;
+  plane.position.set(0, 30, 0);
+  group.add(plane);
 
+  const plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = 0.5 * Math.PI;
+  plane.position.set(0, -30, 0);
+  group.add(plane2);
+
+  //Light
   const light = new THREE.PointLight(0xffffff, 1, 100);
   light.position.set(0, 10, 10);
   scene.add(light);
+
+  const ambientLight = new THREE.AmbientLight(0xaaaaaa);
+  scene.add(ambientLight);
+  
+  scene.add(group);
 
   //Controls
   const controls = new OrbitControls(camera, node);
@@ -59,7 +65,7 @@ const initThreeJsScene = (node: HTMLDivElement) => {
   controls.enablePan = false;
   controls.enableZoom = false;
   controls.autoRotate = true;
-  controls.autoRotateSpeed = 3;
+  controls.autoRotateSpeed = 1;
 
   //Resize
   window.addEventListener('resize', () => {
@@ -81,88 +87,94 @@ const initThreeJsScene = (node: HTMLDivElement) => {
   animate();
 
   // Audio
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
+  const sound = new THREE.Audio(listener);
+  sound.autoplay = true;
+  sound.setLoop(true);
 
-  const initializeAudio = () => {
-    const listener = new THREE.AudioListener();
-    camera.add(listener);
-    const sound = new THREE.Audio(listener);
-    sound.autoplay = true;
+  const audioContext = new AudioContext();
+  const audioElement = document.querySelector('audio') as HTMLMediaElement;
+  const track = audioContext.createMediaElementSource(audioElement);
+  const analyser = audioContext.createAnalyser();
+  const gainNode = audioContext.createGain();
+  const pannerOptions = { pan: 0 };
+  const panner = new StereoPannerNode(audioContext, pannerOptions);
+  track.connect(gainNode)
+    .connect(panner)
+    .connect(analyser)
+    .connect(audioContext.destination);
+  analyser.fftSize = 512;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
 
-    const audioContext = new AudioContext();
-    const audioElement = document.querySelector('audio') as HTMLMediaElement;
-    const gainNode = audioContext.createGain();
-    const pannerOptions = { pan: 0 };
-    const panner = new StereoPannerNode(audioContext, pannerOptions);
-    const track = audioContext.createMediaElementSource(audioElement);
-    track.connect(gainNode).connect(panner).connect(audioContext.destination);
 
-    const playButton = document.getElementById('play-button');
-    if (playButton) {
-      playButton?.addEventListener(
-        "click",
-        () => {
-          // Check if context is in suspended state (autoplay policy)
-          if (audioContext.state === "suspended") {
-            audioContext.resume();
-          }
+  const playButton = document.getElementById('play-button');
+  if (playButton) {
+    playButton?.addEventListener(
+      "click",
+      () => {
+        // Check if context is in suspended state (autoplay policy)
+        if (audioContext.state === "suspended") {
+          audioContext.resume();
+        }
 
-          // Play or pause track depending on state
-          if (playButton.dataset.playing === "false") {
-            audioElement.play();
-            playButton.dataset.playing = "true";
-          } else if (playButton.dataset.playing === "true") {
-            audioElement.pause();
-            playButton.dataset.playing = "false";
-          }
-        },
-        false,
-      );
+        // Play or pause track depending on state
+        if (playButton.dataset.playing === "false") {
+          audioElement.play();
+          playButton.dataset.playing = "true";
+        } else if (playButton.dataset.playing === "true") {
+          audioElement.pause();
+          playButton.dataset.playing = "false";
+        }
+      },
+      false,
+    );
 
-      audioElement.addEventListener(
-        'ended',
-        () => {
-          playButton.dataset.playing = 'false';
-        },
-        false,
-      );
-    } else {
-      throw new Error('no audio element');
-    }
-
-    const volumeControl: HTMLInputElement | null = document.querySelector('#volume');
-    if (volumeControl) {
-      volumeControl.addEventListener(
-        'input',
-        () => {
-          gainNode.gain.value = parseFloat(volumeControl.value);
-        },
-        false,
-      );
-    } else {
-      throw new Error('no volume control');
-    }
-
-    const pannerControl: HTMLInputElement | null = document.querySelector('#panner');
-
-    if (pannerControl) {
-      pannerControl.addEventListener(
-        'input',
-        () => {
-          panner.pan.value = parseFloat(pannerControl.value);
-        },
-        false,
-      );
-    } else {
-      throw new Error('no panner control');
-    }
-
+    audioElement.addEventListener(
+      'ended',
+      () => {
+        playButton.dataset.playing = 'false';
+      },
+      false,
+    );
+  } else {
+    throw new Error('no audio element');
   }
-  initializeAudio();
+
+  const volumeControl: HTMLInputElement | null = document.querySelector('#volume');
+  if (volumeControl) {
+    volumeControl.addEventListener(
+      'input',
+      () => {
+        gainNode.gain.value = parseFloat(volumeControl.value);
+      },
+      false,
+    );
+  } else {
+    throw new Error('no volume control');
+  }
+
+  const pannerControl: HTMLInputElement | null = document.querySelector('#panner');
+
+  if (pannerControl) {
+    pannerControl.addEventListener(
+      'input',
+      () => {
+        panner.pan.value = parseFloat(pannerControl.value);
+      },
+      false,
+    );
+  } else {
+    throw new Error('no panner control');
+  }
+
+
 
   //Timeline
   const tl = gsap.timeline({ defaults: { duration: 1 } })
   const tl2 = gsap.timeline({ defaults: { duration: 3 } })
-  tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
+  tl.fromTo(sphere.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
   tl2.fromTo('nav', { y: '-100%' }, { y: '0%' })
   tl2.fromTo('.title', { opacity: 0 }, { opacity: 1 })
 
@@ -184,7 +196,7 @@ const initThreeJsScene = (node: HTMLDivElement) => {
       ]
       // Let's animate
       const newColor = new THREE.Color(rgbToHex(rbg[0], rbg[1], rbg[2]));
-      gsap.to(mesh.material.color, {
+      gsap.to(sphere.material.color, {
         r: newColor.r,
         g: newColor.g,
         b: newColor.b,
