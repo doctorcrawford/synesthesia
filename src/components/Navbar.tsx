44e6b30d25f89ed0a0ea78613f2f91a17a5cbeb6
Kyle Crawford <kcraw217@gmail.com>
2023-07-21 09:39:12 -0700

    add spotify links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,8 @@ function Navbar() {
         <a href="/">Synesthesia</a>
         <ul>
           <li>Sign In</li>
-          <li>Pick a Song</li>
+          <a href="/spotify"><li>Spotify</li></a>
+          <a href="/webplayback"><li>Pick a Song</li></a>
         </ul>
       </nav>
     </>
