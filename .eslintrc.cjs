4aa24f7bccc14df884df4f181bbd57fc4b28a8d2
Kyle Crawford <kcraw217@gmail.com>
2023-07-25 14:02:35 -0700

    change unused variables to 'warn'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 }
