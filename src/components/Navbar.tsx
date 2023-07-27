1fcef5a4e7174620db3fcca68d5d70d3b11eb975
Kyle Crawford <kcraw217@gmail.com>
2023-07-27 10:59:18 -0700

    comment out links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ function Navbar() {
       <nav>
         <a href="/">Synesthesia</a>
         <ul>
-          <li>Sign In</li>
-          <a href="/spotify"><li>Spotify</li></a>
-          <a href="/webplayback"><li>Pick a Song</li></a>
+          {/* <li>Sign In</li> */}
+          {/* <a href="/spotify"><li>Spotify</li></a>
+          <a href="/webplayback"><li>Pick a Song</li></a> */}
         </ul>
       </nav>
     </>
