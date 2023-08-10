b94317aa90996b49bfe650b3deff1f212dc41639
Kyle Crawford <kcraw217@gmail.com>
2023-08-10 14:02:33 -0700

    add progressbar styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './styles/styles.css'
+import './styles/styles.css';
+import './styles/customize-progress-bar.css';
 
 const element = document.getElementById('root');
 if (element === null) throw new Error('Root container missing in index.html');
