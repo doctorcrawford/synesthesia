5903acf02ba8df06ef8b16e94f341e44bb2b7eb0
Kyle Crawford <kcraw217@gmail.com>
2023-08-11 21:34:15 -0700

    add ronnie B

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,11 +1,18 @@
-import track from './../resources/radio-man.mp3';
-import thumbnail from './../resources/radio-man-thumbnail.png';
+import radioMan from './../resources/radio-man.mp3';
+import radioManPic from './../resources/radio-man-thumbnail.png';
+import ronnieB from './../resources/ronnie-b.mp3';
 
 export const tracks = [
   {
     title: 'Radio Man ft. An',
-    src: track,
+    src: radioMan,
     author: 'KNXN ft. An',
-    thumbnail: thumbnail,
-  }
+    thumbnail: radioManPic,
+  },
+  {
+    title: 'Ronnie B',
+    src: ronnieB,
+    author: 'KNXN',
+    // thumbnail: ,
+  },
 ]
